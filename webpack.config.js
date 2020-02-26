34a61a3385ab060c0eff62d1be635ef3de4213fa
Alejandro Crispin <alejandro.crisin@gnp.com.mx>
2020-02-26 12:54:36 -0600

    @Add working with separate state manager.

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const config = {
   resolve: {
     modules: [
       path.resolve('./lib'),
-      path.resolve('./node_modudles')
+      path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill', './lib/components/Index.js'],
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
